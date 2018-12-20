20ac9d4c83142e9e4ec87dd06e0f0b96e5b41857
Madura De Silva <Madura.D@CreativeSoftware.com>
2018-12-20 16:42:42 +0530

    basic implementation done

---
+++ AzureVision/AzureVision/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using Plugin.Media;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AzureVision
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async Task<AnalysisResult> GetImageDescription(Stream imageStream)
+        {
+            VisionServiceClient visionClient = new VisionServiceClient("0d2957b6055d42598f1b4588c8f7282d", "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0");
+            VisualFeature[] features = { VisualFeature.Tags, VisualFeature.Categories, VisualFeature.Description };
+            return await visionClient.AnalyzeImageAsync(imageStream, features.ToList(), null);
+        }
+        
+        private async void BtnPick_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            try
+            {
+                var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+                if (file == null)
+                    return;
+                imgChoosed.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+                var result = await GetImageDescription(file.GetStream());
+                lblResult.Text = null;
+                file.Dispose();
+                foreach (string tag in result.Description.Tags)
+                {
+                    lblResult.Text = lblResult.Text + "\n" + tag;
+                }
+            }
+            catch
+            (Exception ex)
+            {
+                string test = ex.Message;
+            }
+        }
+        
+        private async void BtnTake_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            try
+            {
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "xamarin.jpg"
+                });
+                if (file == null)
+                    return;
+                imgChoosed.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+                var result = await GetImageDescription(file.GetStream());
+                file.Dispose();
+                lblResult.Text = null;
+                //lblResult.Text = result.Description.Captions.First().Text;
+                foreach (string tag in result.Description.Tags)
+                {
+                    lblResult.Text = lblResult.Text + "\n" + tag;
+                }
+            }
+            catch (Exception ex)
+            {
+                string test = ex.Message;
+            }
+        }
+    }
+}
